fd5d0a93f35f0d9cfd2cb85281a339e3b1c3bbb0
Buster Neece <buster@busterneece.com>
2024-03-20 10:39:57 -0500

    Switch to AzuraCast library for Doctrine entity normalizer.

--- backend/src/Entity/Artist.php
+++ backend/src/Entity/Artist.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use App\Normalizer\Attributes\DeepNormalize;
+use Azura\Normalizer\Attributes\DeepNormalize;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
